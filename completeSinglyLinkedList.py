8a2dc844c416da551bf764ea194a952df4a54638
waseem <v867610@gmail.com>
2025-09-28 18:45:12 +0530

    completeSinglyLL

---
+++ completeSinglyLinkedList.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__ (self,data):
+        self.data = data
+        self.next  =None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert_at_beginning(self,new_data):
+        new_node = Node(new_data)
+        new_node.next=self.head
+        self.head = new_node
+       
+    def insert_at_end(self,new_data):
+        new_node = Node(new_data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def insert_after_data(self,target,new_data):
+        new_node = Node(new_data)
+
+        current = self.head
+
+        while current:
+            if current.data == target:
+                new_node.next = current.next
+                current.next = new_node
+                return
+            current = current.next
+
+    def display(self):
+        current = self.head
+        if current is None:
+            print("Linked List is empty")
+            return
+        print ("Linked List: ", end="")
+        
+        while current:
+
+            print(f"{current.data} ->" , end="")
+            current = current.next
+        print("None")
+
+    
+
+my_list = LinkedList()
+
+my_list.insert_at_beginning(20)
+my_list.insert_at_end(10)
+my_list.insert_after_data(20,30)
+
+
+my_list.display()
